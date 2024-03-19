a3e0d7867623ad9ccef3df99109fed9447ad4f48
MaccariLuca <101560742+MaccariLuca@users.noreply.github.com>
2024-03-19 16:27:09 +0100

    Re-update

---
+++ codice/FAKEINVADER/src/main/java/controllerPack/LoginMenuController.java
@@ -0,0 +1,147 @@
+package controllerPack;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import ViewPack.LoginMenuView;
+import database.CreateDB;
+import modelPack.Commons;
+import modelPack.LoginMenuModel;
+import modelPack.Menu;
+import modelPack.PlayerDAO;
+import modelPack.SessionManager;
+
+public class LoginMenuController {
+
+	private LoginMenuModel model;
+	private LoginMenuView view;
+	LoginMenuController() throws IOException
+	{
+		JFrame frame = new JFrame();
+		frame.setContentPane(new JPanel() 
+		{			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			
+			File pathmenu  = new File("src/main/java/images/menu.jpg");
+			BufferedImage image = ImageIO.read(pathmenu);
+			
+	        public void paintComponent(Graphics g) 
+	        {
+	            super.paintComponent(g);
+	            g.drawImage(image, -45, -100, 768, 768, this);
+	        }
+	    });
+		
+		
+		//Listener 
+		
+		buttonLogin.addActionListener(e -> {
+		    try {
+		        // Ottieni l'istanza del PlayerDAO utilizzando la connessione al database dal Singleton
+		        PlayerDAO playerDAO = new PlayerDAO(CreateDB.getInstance().getConnection());
+
+		        // Verifica delle credenziali dell'utente nel database
+		        String usernameInput = username.getText();
+		        String passwordInput = password.getText();
+
+		        if (playerDAO.checkCredentials(usernameInput, passwordInput)) {
+		            // Se le credenziali sono corrette, imposta l'utente corrente e apri il menu
+		            SessionManager.setCurrentUsername(usernameInput);
+		            frame.dispose();
+		            try {
+		                new Menu();
+		            } catch (IOException e1) {
+		                e1.printStackTrace();
+		            }
+		        } else {
+		            // Se le credenziali non sono corrette, mostra un messaggio di errore
+		            System.out.println("Utente non registrato");
+		            error.setText("Attention: unregistered user!");
+		        }
+		    } catch (SQLException ex) {
+		        // Gestione delle eccezioni
+		        ex.printStackTrace();
+		    } finally {
+		        // Resetta i campi indipendentemente dal risultato della query
+		        username.setText("");
+		        password.setText("");
+		    }
+		});
+
+
+		buttonRegistration.addActionListener(e -> {
+		    try (Connection conn =CreateDB.getInstance().getConnection()) {
+		        if (conn != null) {
+		            DatabaseMetaData meta = conn.getMetaData();
+		            System.out.println("The driver name is " + meta.getDriverName());
+		        }
+
+		        // Validazione dell'input
+		        String inputUsername = username.getText();
+		        String inputPassword = password.getText();
+
+		        if (inputUsername.isEmpty() || inputPassword.isEmpty()) {
+		            System.out.println("Attenzione: Inserire username e password");
+		            error.setText("Attention: Enter your username and password");
+		        } else {
+		            try {
+		                // Creazione del PlayerDAO utilizzando la connessione al database
+		                PlayerDAO playerDAO = new PlayerDAO(conn);
+
+		                // Verifica se l'utente è già registrato
+		                if (playerDAO.playerExists(inputUsername)) {
+		                    System.out.println("Utente già registrato");
+		                    username.setText("");
+		                    password.setText("");
+		                    error.setText("Attention: User already registered!");
+		                } else {
+		                    // Inserisci il nuovo utente
+		                    if (playerDAO.insertPlayer(inputUsername, inputPassword)) {
+		                        System.out.println("Utente inserito con successo");
+		                        frame.dispose();
+		                        new Menu();
+		                    } else {
+		                        System.out.println("Errore durante l'inserimento dell'utente");
+		                        error.setText("Error while registering user");
+		                    }
+		                }
+		            } catch (SQLException ex) {
+		                ex.printStackTrace();
+		                // Gestione degli errori SQL
+		                System.out.println("Errore SQL: " + ex.getMessage());
+		                error.setText("Error while accessing database");
+		            }
+		        }
+		    } catch (SQLException ex) {
+		        ex.printStackTrace();
+		        // Gestione degli errori SQL
+		        System.out.println("Errore SQL: " + ex.getMessage());
+		        error.setText("Error while accessing database");
+		    } catch (Exception ex) {
+		        ex.printStackTrace();
+		        // Gestione di altri tipi di errori
+		        System.out.println("Errore: " + ex.getMessage());
+		        error.setText("Unknown error");
+		    }
+		});
+
+	}
+}
---
+++ codice/FAKEINVADER/src/main/java/modelPack/FakeInvaders.java
@@ -0,0 +1,32 @@
+package modelPack;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import ViewPack.Board;
+
+public class FakeInvaders extends JFrame  {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FakeInvaders() 
+	{
+		var board = new Board();
+        add(board);
+
+        setTitle("FAKE INVADERS");
+        setSize(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        ImageIcon icon = new ImageIcon("src/main/java/images/ship.png");
+		setIconImage(icon.getImage());
+        
+    }
+ 
+}
