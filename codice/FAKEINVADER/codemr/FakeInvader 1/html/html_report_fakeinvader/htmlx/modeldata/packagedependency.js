f8dd78244418408012a232337eb6db74d560b595
SickCeco <francesco.tomasoni96@gmail.com>
2024-03-20 18:06:49 +0100

    codeRM analysis and upgrade documentation

---
+++ codice/FAKEINVADER/codemr/FakeInvader 1/html/html_report_fakeinvader/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,11,0],[0,0,0,0,0],[11,0,0,10,0],[0,3,0,0,0],[0,0,1,0,0]]
+var packages = [{
+"name": " ViewPack", "color": " #3182bd"
+}
+,{
+"name": " database", "color": " #6baed6"
+}
+,{
+"name": " controllerPack", "color": " #9ecae1"
+}
+,{
+"name": " modelPack", "color": " #c6dbef"
+}
+,{
+"name": " Main", "color": " #e6550d"
+}
+];
